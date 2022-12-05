749cf376a1dec9f5b63b3c93bfddc600fc9f9671
Okan <okanacioglu@gmail.com>
2022-12-05 22:52:00 +0300

    birthday-reminder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import data from './data'
+import List from './List'
 function App() {
+  const [birthdays, setBirthdays] = useState(data)
+  const clearHandler = () => {
+    setBirthdays((birthdays) => {
+      return (birthdays = [])
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <main>
+      <section className='container'>
+        <h3>{birthdays.length} birthdays today</h3>
+        <List birthdays={birthdays} />
+        <button className='btn' onClick={clearHandler}>
+          Clear All
+        </button>
+      </section>
+    </main>
+  )
 }
 
-export default App;
+export default App
