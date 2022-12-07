a38aa04258117eb843057997baf6869af564b12a
Okan <okanacioglu@gmail.com>
2022-12-07 18:58:05 +0300

    adding-categories-button-dynamically

--- menu/src/App.js
+++ menu/src/App.js
@@ -4,13 +4,21 @@ import Categories from './Categories'
 import items from './data'
 //! For the button names we will use category property so that they will be dynamic. More category we have more buttons will be appear.
 
+//! For take the unique values we will use Set...
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
 function App() {
   //! We will use two state: one for menu items and one for categories
   const [menuItems, setMenuItems] = useState(items)
-  const [categories, setCategories] = useState([])
+  const [categories, setCategories] = useState(allCategories)
 
   //! We will filter the list and We will iterate on original items...
   const filterItems = (category) => {
+    //! Condition to show all items again
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
     const newItems = items.filter((item) => item.category === category)
     setMenuItems(newItems)
   }
@@ -21,7 +29,7 @@ function App() {
           <h2>Our Menu</h2>
           <div className='underline'></div>
         </div>
-        <Categories filterItems={filterItems} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
--- menu/src/Categories.js
+++ menu/src/Categories.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
-const Categories = ({ filterItems }) => {
-  //! We will add buttons manually
+const Categories = ({ categories, filterItems }) => {
+  //! We will add buttons dynamically
   return (
     <div className='btn-container'>
-      <button
-        className='filter-btn'
-        onClick={() => {
-          filterItems('breakfast')
-        }}
-      >
-        breakfast
-      </button>
+      {categories.map((category, index) => {
+        return (
+          <button
+            type='button'
+            className='filter-btn'
+            key={index}
+            onClick={() => filterItems(category)}
+          >
+            {category}
+          </button>
+        )
+      })}
     </div>
   )
 }
