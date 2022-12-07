a1c48fe5b7c9975809c5a5bb21dc27da969f1b7e
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-07 13:03:20 +0300

    prev-and-next

--- reviews/src/App.js
+++ reviews/src/App.js
@@ -1,7 +1,29 @@
-import React from 'react';
-import Review from './Review';
+import React from 'react'
+import Review from './Review'
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <>
+      <main>
+        <section className='container'>
+          <div className='title'>
+            <h2>our Reviews</h2>
+            <div className='underline'></div>
+          </div>
+          <Review />
+        </section>
+      </main>
+    </>
+  )
 }
 
-export default App;
+export default App
+
+//! We have some icons in this project. We can technically use font awesome by adding it into the index.html. But we will use React Icons Library.
+//! Icons that we use using React Icon Library are components.
+//! What good is that we can add them className or style.
+//! Also they are added as "svg" as HTML element.
+
+//! Download React-Icons --> npm install react-icons
+//! In order to use icon we would need to import specific icon from specific library. We can search specific icons in the website "https://react-icons.github.io/react-icons/"
+//! import {FaGithub} from "react-icons/fa" --> if we want to use "FaGithub" we need to add "fa" at the end of the library as "react-icons/fa"
--- reviews/src/Review.js
+++ reviews/src/Review.js
@@ -1,9 +1,71 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from 'react'
+import people from './data'
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa'
 
 const Review = () => {
-  return <h2>review component</h2>;
-};
+  //! We use "0" as initial value because array are zero index based.
+  //! By default we will show first person
+  const [index, setIndex] = useState(0)
 
-export default Review;
+  //! We will destructing the data but we will manipulate it with using index state. With this way as the index value change we can change the people that we want to show.
+  const { name, job, image, text } = people[index]
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0
+    }
+    if (number < 0) {
+      return people.length - 1
+    }
+    return number
+  }
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex
+      //! Alternative way without checkNumber function
+      // if (index === people.length - 1) {
+      //   return (newIndex = 0)
+      // }
+      newIndex = index + 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex
+      //! Alternative way without checkNumber function
+      // if (index === 0) {
+      //   return (newIndex = people.length - 1)
+      // }
+      newIndex = index - 1
+      return checkNumber(newIndex)
+    })
+  }
+
+  return (
+    <article className='review'>
+      <div className='img-container'>
+        <img src={image} alt={name} className='person-img' />
+        <span className='quote-icon'>
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className='author'>{name}</h4>
+      <p className='job'>{job}</p>
+      <p className='info'>{text}</p>
+      <div className='button-container'>
+        <button className='prev-btn' onClick={prevPerson}>
+          <FaChevronLeft />
+        </button>
+        <button className='next-btn' onClick={nextPerson}>
+          <FaChevronRight />
+        </button>
+      </div>
+      <button className='random-btn'>suprise me</button>
+    </article>
+  )
+}
+
+export default Review
