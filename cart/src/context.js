b89ef35bdf5652e2eff4cd259bcb917258a39250
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-13 14:13:49 +0300

    refactor-increase-decrease

--- cart/src/context.js
+++ cart/src/context.js
@@ -39,6 +39,9 @@ const AppProvider = ({ children }) => {
     const cart = await response.json()
     dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
   }
+  const toggleAmount = (id, type) => {
+    dispatch({ type: 'TOGGLE_AMOUNT', payload: { id, type } })
+  }
 
   useEffect(() => {
     fetchData()
@@ -58,6 +61,7 @@ const AppProvider = ({ children }) => {
         remove,
         increase,
         decrease,
+        toggleAmount,
       }}
     >
       {children}
