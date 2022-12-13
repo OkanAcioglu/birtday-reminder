79426085d89a8492e48c676eb930fd09375b231f
Okan Acıoğlu <okanacioglu@gmail.com>
2022-12-13 13:29:53 +0300

    fetching-data

--- cart/src/context.js
+++ cart/src/context.js
@@ -33,9 +33,19 @@ const AppProvider = ({ children }) => {
     dispatch({ type: 'DECREASE_ITEM', payload: id })
   }
 
+  const fetchData = async () => {
+    dispatch({ type: 'LOADING' })
+    const response = await fetch(url)
+    const cart = await response.json()
+    dispatch({ type: 'DISPLAY_ITEMS', payload: cart })
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   //! We do not setup a new function instead we will use useEffect
   //! Everytime our value in state changes we would want to call that useEffect
-
   useEffect(() => {
     dispatch({ type: 'GET_TOTALS' })
   }, [state.cart])
