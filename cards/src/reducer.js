ffb9db1d76a442cbd20c95401449c05423676152
Okan <okanacioglu@gmail.com>
2022-12-13 05:33:53 +0300

    clear-cart

--- cards/src/reducer.js
+++ cards/src/reducer.js
@@ -1,6 +1,11 @@
 //! setup function export it and grab it into the context
 
 const reducer = (state, action) => {
+  if (action.type === 'CLEAR_CART') {
+    return { ...state, cart: [] }
+  }
   return state
 }
 export default reducer
+
+//! Some people prefer to use switch statement...
