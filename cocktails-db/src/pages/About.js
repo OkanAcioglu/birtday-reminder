e66cdf71304c4bbce1310c2a17a5c3e8a3525817
Okan <okanacioglu@gmail.com>
2022-12-31 01:54:08 +0300

    finish-about-error-pages

--- cocktails-db/src/pages/About.js
+++ cocktails-db/src/pages/About.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quo
+        excepturi laborum non quis ducimus? Quae, eligendi beatae earum
+        perspiciatis culpa vero minima praesentium facilis aut quaerat at cumque
+        repudiandae?
+      </p>
+    </section>
   )
 }
 
