8dc887676cefe5c4e2b55ea962de6ef6fb8446b7
Okan Acıoğlu <okanacioglu@gmail.com>
2023-01-03 11:41:59 +0300

    fetch-display-list

--- pagination/src/App.js
+++ pagination/src/App.js
@@ -1,8 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { useFetch } from './useFetch'
 import Follower from './Follower'
+
+//! When it comes to pagination there is 2 option...
+//* Pagination on frontend --> request data whether it is 30 or 100 --> Then paginate it in our end
+//* Pagination on a server --> request different pages from the server --> Already paginated on the server --> setting request for those different pages
+//? This project is pagination on frontend
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch()
+
+  return (
+    <main>
+      <div className='section-title'>
+        <h1>{loading ? 'loading...' : 'pagination'}</h1>
+        <div className='underline'></div>
+        <section className='followers'>
+          <div className='container'>
+            {data.map((follower) => {
+              return <Follower key={follower.id} {...follower} />
+            })}
+          </div>
+        </section>
+      </div>
+    </main>
+  )
 }
 
 export default App
--- pagination/src/Follower.js
+++ pagination/src/Follower.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Follower = () => {
-  return <h2>follower component</h2>
+const Follower = ({ avatar_url, html_url, login }) => {
+  //! We have our list here whether it is 30 or 100 or 5000...
+  return (
+    <article className='card'>
+      <img src={avatar_url} alt={login} />
+      <h4>{login}</h4>
+      <a href={html_url} className='btn'>
+        view profile
+      </a>
+    </article>
+  )
 }
 
 export default Follower
