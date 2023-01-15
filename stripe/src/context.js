c5e451052c50842607b73eff9ffd717ea669d350
Okan <okanacioglu@gmail.com>
2023-01-15 17:54:53 +0300

    links

--- stripe/src/context.js
+++ stripe/src/context.js
@@ -7,7 +7,7 @@ export const AppProvider = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
   const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
   const [location, setLocation] = useState({})
-
+  const [page, setPage] = useState({ page: '', links: [] })
   const openSidebar = () => {
     setIsSidebarOpen(true)
   }
@@ -16,6 +16,8 @@ export const AppProvider = ({ children }) => {
     setIsSidebarOpen(false)
   }
   const openSubmenu = (text, coordinates) => {
+    const page = sublinks.find((link) => link.page === text)
+    setPage(page)
     setLocation(coordinates)
     setIsSubmenuOpen(true)
   }
@@ -34,6 +36,7 @@ export const AppProvider = ({ children }) => {
         closeSubmenu,
         closeSidebar,
         location,
+        page,
       }}
     >
       {children}
