4be85c719881a621802aaaed87f814a45929e275
Okan Acıoğlu <okanacioglu@gmail.com>
2023-01-24 14:27:54 +0300

    display-photos

--- stock-photos/src/App.js
+++ stock-photos/src/App.js
@@ -13,17 +13,12 @@ function App() {
   const fetchImages = async () => {
     setLoading(true)
     let url
-    //! Now we are share our key...
-    //! It is much better create a environment variables and then add that env file to the git ignore. So when we push it to the github, we can only see the name of the value.
-    //! Location of the env file is extremly important and DO NOT put it in the src... Just place it in the root
-    //! name must start with REACT_APP_ after that it is up to us...
-    //! Whenever setup the .env file -> restart the dev server
-    //! DO NOT FORGET to add .env file to the gitignore...
     url = `${mainUrl}${clientID}`
     try {
       const response = await fetch(url)
       const data = await response.json()
-      console.log(data)
+      setPhotos(data)
+      setLoading(false)
     } catch (err) {
       setLoading(false)
       console.log(err)
@@ -33,7 +28,39 @@ function App() {
   useEffect(() => {
     fetchImages()
   }, [])
-  return <h2>stock photos starter</h2>
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hello')
+  }
+
+  return (
+    <main>
+      <section className='search'>
+        <form className='search-form'>
+          <input type='text' placeholder='search' className='form-input' />
+          <button type='submit' className='submit-btn' onClick={handleSubmit}>
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className='photos'>
+        <div className='photos-center'>
+          {photos.map((image) => {
+            //console.log(image)
+            return <Photo key={image.id} {...image} />
+          })}
+        </div>
+        {loading && <h2 className='loading'>Loading...</h2>}
+      </section>
+    </main>
+  )
 }
 
 export default App
+
+//! It is much better create a environment variables and then add that env file to the git ignore. So when we push it to the github, we can only see the name of the value.
+//! Location of the env file is extremly important and DO NOT put it in the src... Just place it in the root
+//! name must start with REACT_APP_ after that it is up to us...
+//! Whenever setup the .env file -> restart the dev server
+//! DO NOT FORGET to add .env file to the gitignore...
