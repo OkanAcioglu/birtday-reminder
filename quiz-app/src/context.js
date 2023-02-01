8ba7a59138e26f486be5d7311897baa39cc3eb7e
Okan Acıoğlu <okanacioglu@gmail.com>
2023-02-01 14:33:23 +0300

    setup-form

--- quiz-app/src/context.js
+++ quiz-app/src/context.js
@@ -25,7 +25,12 @@ const AppProvider = ({ children }) => {
   const [index, setIndex] = useState(0)
   const [correct, setCorrect] = useState(0)
   const [error, setError] = useState(false)
-
+  //! below state names are important... need to equally match...
+  const [quiz, setQuiz] = useState({
+    amount: 10,
+    category: 'sports',
+    difficulty: 'easy',
+  })
   const [isModalOpen, setIsModalOpen] = useState(false)
 
   const fetchQuestions = async (url) => {
@@ -76,9 +81,13 @@ const AppProvider = ({ children }) => {
     setIsModalOpen(false)
   }
 
-  useEffect(() => {
-    fetchQuestions(tempUrl)
-  }, [])
+  const handleChange = (e) => {
+    console.log(e)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
 
   return (
     <AppContext.Provider
@@ -93,6 +102,9 @@ const AppProvider = ({ children }) => {
         nextQuestion,
         checkAnswer,
         closeModal,
+        quiz,
+        handleSubmit,
+        handleChange,
       }}
     >
       {children}
